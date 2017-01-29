ff252d57297386d824b08490841730870f20ffad
Valentin <contact@eywek.fr>
2017-01-29 17:49:02 +0100

    fix purchase with points

--- Controller/PurchaseController.php
+++ Controller/PurchaseController.php
@@ -33,7 +33,7 @@ class PurchaseController extends PlayerMarketAppController {
       return $this->response->body(json_encode(array('status' => false, 'msg' => "Vous êtes déconnecté. Il est donc impossible de procéder à un achat.")));
     if (!$this->Server->online($this->serverId))
       return $this->response->body(json_encode(array('status' => false, 'msg' => "Le serveur est temporairement indisponible. Il est donc impossible de procéder à un achat.")));
-    $this->Server->call(array('performCommand' => "market give {$this->User->getKey('pseudo')} {$find['Sale']['id_selling']} POINTS"), true, $this->serverId);
+   $this->Server->call(array('performCommand' => "market give {$this->User->getKey('pseudo')} {$find['Sale']['id_selling']} POINT"), true, $this->serverId);
 
     sleep(1); // wait plugin db update
 
@@ -48,7 +48,7 @@ class PurchaseController extends PlayerMarketAppController {
 
     // Calculate new sold
     $findUser = $this->User->find('first', array('conditions' => array('id' => $this->User->getKey('id'))));
-    $newSold = floatval($findUser['User']['money']) - floatval($find['Sale']['price_money']);
+    $newSold = floatval($findUser['User']['money']) - floatval($find['Sale']['price_point']);
 
     // Set new sold
     $this->User->id = $this->User->getKey('id');
@@ -58,12 +58,12 @@ class PurchaseController extends PlayerMarketAppController {
     $this->api = $this->Components->load('Obsi.Api');
     $getUsername = $this->api->get('/user/from/uuid/' . $find['Sale']['seller']);
     if (!$getUsername->status || !$getUsername->success)
-      return false;
+      return $this->response->body(json_encode(array('status' => false, 'msg' => "Une erreur est intervenue lors de l'achat (2). Veuillez rééssayer.")));
     $usernamme = $getUsername->body['username'];
 
     // Calculate new sold
     $findUser = $this->User->find('first', array('conditions' => array('pseudo' => $username)));
-    $newSold = floatval($findUser['User']['money']) - floatval($find['Sale']['price_money']);
+    $newSold = floatval($findUser['User']['money']) + floatval($find['Sale']['price_point']);
 
     // Set new sold
     $this->User->id = $findUser['User']['id'];
